d8edfa865e7a7d79e62d4e53bca34e3d923ea5ea
Kinjo.Takumi <tkinjo1@gmail.com>
2020-07-01 15:42:28 +0900

    change database config

--- config/settings.py
+++ config/settings.py
@@ -76,17 +76,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-db_from_env = dj_database_url.config()
 DATABASES = {
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# db_from_env = dj_database_url.config()
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
 
 
 # Password validation
