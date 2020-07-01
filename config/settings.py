a99fe145451562f5e4bfb74f2a92244b5724ee7d
Kinjo.Takumi <tkinjo1@gmail.com>
2020-07-01 10:02:39 +0900

    manual deploy

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@mo2+rki#*%^p@z86#)6^u*g+07+dor-_(&hszdr9ys2qxwjx5'
+# SECRET_KEY = '@mo2+rki#*%^p@z86#)6^u*g+07+dor-_(&hszdr9ys2qxwjx5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
