069a67287fbb190b305474277d5df1dfe9ed7bcb
Kinjo.Takumi <tkinjo1@gmail.com>
2020-07-06 19:44:55 +0900

    deploy test

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("デプロイ")
+    return HttpResponse("Container Registry & Runtime (Docker Deploys)")
